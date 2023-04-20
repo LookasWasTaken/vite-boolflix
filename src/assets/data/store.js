a56e45d2130942cf032bc362b85a0e3d35aa81f5
LookasWasTaken~ <piacen.luca@gmail.com>
2023-04-20 11:28:47 +0200

    ADD cover img and method for fetching tv shows

--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -3,16 +3,33 @@ import axios from "axios";
 
 export const store = reactive({
   loading: true,
-  API_URL: "https://api.themoviedb.org/3/search/movie",
+  API_URL: "https://api.themoviedb.org/3/",
   API_KEY: "5cb6e91b738a69ca58008c188eeebb3f",
+  pathMovies: "search/movie",
+  pathSeries: "search/tv",
+  pathCover: "https://image.tmdb.org/t/p/w342",
   search: '',
   movies: [],
+  series: [],
   fetchMovie() {
-    let url = `${this.API_URL}?api_key=${this.API_KEY}&query=${this.search.toLowerCase()}`;
+    let url = `${this.API_URL + this.pathMovies}?api_key=${this.API_KEY}&query=${this.search.toLowerCase()}`;
     axios
       .get(url)
       .then((response) => {
         this.movies = response.data.results;
+        this.loading = false;
+      })
+      .catch((err) => {
+        console.log(err);
+        console.error(err.messagge);
+      });
+  },
+  fetchSeries() {
+    let url = `${this.API_URL + this.pathSeries}?api_key=${this.API_KEY}&query=${this.search.toLowerCase()}`;
+    axios
+      .get(url)
+      .then((response) => {
+        this.series = response.data.results;
         this.loading = false;
       })
       .catch((err) => {
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -33,25 +33,46 @@ export default {
 <template>
     <header>
         <nav class="d-flex justify-content-between align-items-center">
-            <h1>Netflix</h1>
+            <div class="logo">
+                <h1>Netflix</h1>
+            </div>
             <div class="search">
                 <div class="input-group mb-3">
-                    <input @keyup.enter="store.fetchMovie()" type="text" class="form-control" placeholder="Search Movie"
-                        aria-label="Search Movie" aria-describedby="button-addon2" v-model.trim="store.search">
-                    <button @click="store.fetchMovie()" class="btn btn-outline-secondary" type="button"
+                    <input @keyup.enter="store.fetchMovie(), store.fetchSeries()" type="text" class="form-control"
+                        placeholder="Search Movie" aria-label="Search Movie" aria-describedby="button-addon2"
+                        v-model.trim="store.search">
+                    <button @click="store.fetchMovie(), store.fetchSeries()" class="btn btn-outline-secondary" type="button"
                         id="button-addon2">Search</button>
                 </div>
             </div>
         </nav>
         <div class="container">
-            <ul>
+            <ul v-if="store.movies.length > 0">
+                <li>
+                    <h2>Movies</h2>
+                </li>
                 <li v-for="(movie, index) in store.movies">
+                    <img v-if="movie.poster_path" :src="`${store.pathCover}${movie.poster_path}`" :alt="`${movie.title} cover image`">
                     <h2>Movie {{ index }}</h2>
                     <p>Title: {{ movie.title }}</p>
-                    <p>Original title: {{ movie.original_title }}</p>
+                    <p v-if="movie.title != movie.original_title">Original title: {{ movie.original_title }}</p>
                     <p>Language: {{ movie.original_language }} <country-flag :country="setLanguage(movie.original_language)"
                             size='normal' :rounded="true" :shadow="true" /></p>
                     <p>Score: {{ movie.vote_average }}</p>
+                </li>
+            </ul>
+            <ul v-if="store.series.length > 0">
+                <li>serie
+                    <h2>Series</h2>
+                </li>
+                <li v-for="(serie, index) in store.series">
+                    <img v-if="serie.poster_path" :src="`${store.pathCover}${serie.poster_path}`" :alt="`${serie.name} cover image`">
+                    <h2>Serie {{ index }}</h2>
+                    <p>Title: {{ serie.name }}</p>
+                    <p v-if="serie.name != serie.original_name">Original title: {{ serie.original_name }}</p>
+                    <p>Language: {{ serie.original_language }} <country-flag :country="setLanguage(serie.original_language)"
+                            size='normal' :rounded="true" :shadow="true" /></p>
+                    <p>Score: {{ serie.vote_average }}</p>
                 </li>
             </ul>
         </div>
@@ -63,5 +84,8 @@ header {
     color: white;
     background-color: black;
     padding: 1rem;
-}
-</style>
+
+    ul {
+        list-style: none;
+    }
+}</style>
